6fa66cf98b79c3aaadb0e34ddaec215cc78ba322
JohnSmith <salapin.danil1@mail.ru>
2019-12-05 20:04:35 +0000

    fixed some issues

--- task2/webpack.config.js
+++ task2/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlReplaceWebpackPlugin = require('html-replace-webpack-plugin');
 
 
 module.exports = {
@@ -12,8 +13,18 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
+      inject: false
     }),
+    new HtmlReplaceWebpackPlugin([{
+        pattern: '<link rel="stylesheet" href="styles.css">',
+        replacement: '<link rel="stylesheet" href="app.min.css">'
+      },
+      {
+        pattern: ' <script src="script.js"></script>',
+        replacement: ' <script src="app.min.js"></script>'
+      },
+    ]),
     new MiniCssExtractPlugin({
       filename: 'app.min.css',
     }),
